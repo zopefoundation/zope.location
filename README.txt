d29a4d5a375758f17c31a0040ad43245196abd60
Baiju Muthukadan <baiju.m.mail@gmail.com>
2006-10-22 12:36:28 +0000

    Initial commit of zope.location for eggification.

---
+++ README.txt
@@ -0,0 +1,37 @@
+zope.location Package Readme
+============================
+
+Overview
+--------
+
+In Zope3, location are special objects that has a structural location.
+
+Changes
+-------
+
+See CHANGES.txt.
+
+Installation
+------------
+
+See INSTALL.txt.
+
+
+Developer Resources
+-------------------
+
+- Subversion browser:
+
+  http://svn.zope.org/zope.location/
+
+- Read-only Subversion checkout:
+
+  $ svn co svn://svn.zope.org/repos/main/zope.location/trunk
+
+- Writable Subversion checkout:
+
+  $ svn co svn+ssh://userid@svn.zope.org/repos/main/zope.location/trunk
+
+- Note that the 'src/zope/location package is acutally a 'svn:externals' link
+  to the corresponding package in the Zope3 trunk (or to a specific tag, for
+  released versions of the package).
---
+++ setup.py
@@ -0,0 +1,51 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.location package
+
+$Id$
+"""
+
+import os
+
+try:
+    from setuptools import setup, Extension
+except ImportError, e:
+    from distutils.core import setup, Extension
+
+setup(name='zope.location',
+      version='3.4-dev',
+      url='http://svn.zope.org/zope.location',
+      license='ZPL 2.1',
+      description='Zope Proxies',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description="In Zope3, location are special objects"
+                       "that has a structural location.",
+
+      packages=['zope', 'zope.location'],
+      package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      tests_require = ['zope.testing'],
+      install_requires=['zope.interface',
+                        'zope.component',
+                        'zope.proxy',
+                        'zope.schema',
+                        'zope.testing',
+                        'zope.traversing',
+                        'zope.app.component'],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
