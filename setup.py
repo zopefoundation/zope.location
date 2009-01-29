55971251a9483a9645705b735b30ed9079a31103
Christian Theune <ct@gocept.com>
2009-01-29 19:39:33 +0000

    Bump major version because of dependency changes.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*rnames):
     return text
 
 setup(name='zope.location',
-      version = '3.4.1dev',
+      version = '3.5dev',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Zope Location',
