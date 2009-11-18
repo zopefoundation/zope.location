1cddf3db268b241c678d43a0a02adccbfcdf32df
Thomas Lotze <tl@gocept.com>
2009-11-18 09:20:26 +0000

    Preparing release 3.7.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(*rnames):
     return text
 
 setup(name='zope.location',
-      version = '3.7.1dev',
+      version = '3.7.1',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Location',
