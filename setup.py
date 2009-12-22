7fb6773b833c63971c6c39d310726670bf3102da
Hanno Schlichting <hanno@hannosch.eu>
2009-12-22 15:23:20 +0000

    Adjusted to testing output caused by new zope.schema.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(*rnames):
     return text
 
 setup(name='zope.location',
-      version = '3.7.2dev',
+      version = '3.8.0dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Location',
