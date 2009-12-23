b2e3aae745775b064ded9b487938cb9c24864b39
Fabio Tranchitella <fabio@tranchitella.eu>
2009-12-23 21:51:52 +0000

    Fixed a typo in the configure.zcml.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(*rnames):
     return text
 
 setup(name='zope.location',
-      version = '3.8.2dev',
+      version = '3.8.2',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Location',
