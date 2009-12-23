15f0b7c78d574b2c8c8407c0822c49cbc70ac82d
Fabio Tranchitella <fabio@tranchitella.eu>
2009-12-23 22:01:14 +0000

    back to development

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(*rnames):
     return text
 
 setup(name='zope.location',
-      version = '3.8.2',
+      version = '3.8.3dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Location',
