a78e4e18ba1012e259b27e18f243775b2f79cfce
Tres Seaver <tseaver@palladion.com>
2012-06-07 15:49:33 +0000

    Prep 4.0.0 release.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(*rnames):
     return text
 
 setup(name='zope.location',
-      version='4.0.0dev',
+      version='4.0.0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Location',
