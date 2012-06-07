4678c74d9e74a9c61c33cf90fc0d5c83ac050c44
Tres Seaver <tseaver@palladion.com>
2012-06-07 15:52:28 +0000

    svb

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(*rnames):
     return text
 
 setup(name='zope.location',
-      version='4.0.0',
+      version='4.0.1dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Location',
