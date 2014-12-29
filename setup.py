a404abeb4e8b727e5f5e03211170554138f964ca
Tres Seaver <tseaver@palladion.com>
2014-12-29 15:48:51 -0500

    Bow to our new PEP 440 overlords.
    [ci skip]

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(*rnames):
     return text
 
 setup(name='zope.location',
-      version='4.0.4dev',
+      version='4.0.4.dev0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Location',
