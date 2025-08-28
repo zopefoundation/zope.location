24e424a99023f37c5dc93b53a782f48775361e4c
Michael Howitz <icemac@gmx.net>
2025-08-28 08:57:47 +0200

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def read(*rnames):
 
 setup(
     name='zope.location',
-    version='5.2.dev0',
+    version='6.0.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.dev',
     description='Zope Location',
